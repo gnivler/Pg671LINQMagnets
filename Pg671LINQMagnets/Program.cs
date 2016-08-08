d5a1245fab74035e5aa424b35c44d36dabb37b7e
gnivler <gnivil@gmail.com>
2016-08-08 10:51:00 -0600

    record time..easy one I guess

--- Pg671LINQMagnets/Program.cs
+++ Pg671LINQMagnets/Program.cs
@@ -10,6 +10,23 @@ namespace Pg671LINQMagnets
     {
         static void Main(string[] args)
         {
+            int[] badgers = { 36, 5, 91, 3, 41, 69, 8 };
+
+            var bears =
+                    from pigeon in badgers
+                    where (pigeon != 36 && pigeon < 50)
+                    orderby pigeon descending
+                    select pigeon + 5;
+
+            var skunks =
+                    from sparrow in bears
+                    select sparrow - 1;
+
+            var weasels =
+                 skunks.Take(3);
+
+
+            Console.WriteLine("Get your kicks on route {0}", weasels.Sum());
         }
     }
 }
